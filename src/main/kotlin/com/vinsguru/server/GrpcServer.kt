8c8cbf3103c422704b1248f5d9379ae0094d7017
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2023-02-20 15:03:18 -0500

    Runing server

---
+++ src/main/kotlin/com/vinsguru/server/GrpcServer.kt
@@ -0,0 +1,14 @@
+package com.vinsguru.server
+
+import io.grpc.ServerBuilder
+
+fun main(args: Array<String>) {
+    val server = ServerBuilder.forPort(6565)
+        .addService(BackService())
+        .build()
+    server.start()
+    server.awaitTermination()
+}
+
+class GrpcServer {
+}
