376e3b23528968c3afbc849f5c53e81546000c3c
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2023-04-03 19:15:03 -0500

    Server Streaming RPC - Money Withdrawal Service Implementation

--- src/main/kotlin/com/vinsguru/server/BackService.kt
+++ src/main/kotlin/com/vinsguru/server/BackService.kt
@@ -3,6 +3,8 @@ package com.vinsguru.server
 import com.vinsguru.models.Balance
 import com.vinsguru.models.BalanceCheckRequest
 import com.vinsguru.models.BankServiceGrpc
+import com.vinsguru.models.WithdrawRequest
+import com.vinsguru.models.Money
 import io.grpc.stub.StreamObserver
 
 class BackService: BankServiceGrpc.BankServiceImplBase() {
@@ -14,5 +16,20 @@ class BackService: BankServiceGrpc.BankServiceImplBase() {
             .build()
         responseObserver?.onNext(balance)
         responseObserver?.onCompleted()
+    }
+
+    override fun withdraw(request: WithdrawRequest?, responseObserver: StreamObserver<Money>?) {
+
+        val accountNumber = request?.accountNumber ?: 0
+        val amount = request?.amount ?: 0
+        val balance = AccountDatabase.getBalance(accountNumber)
+        AccountDatabase.deductBalance(accountNumber, 10)
+        for (i in 0 .. (amount/10)) {
+            val money = Money.newBuilder().setValue(10).build()
+            responseObserver?.onNext(money)
+            Thread.sleep(1000)
+        }
+        responseObserver?.onCompleted()
+
     }
 }
